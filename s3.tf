bcb1257592122f938da12922a71721b5a33af430
ohbster <ohbster@protonmail.com>
2024-02-07 23:27:46 -0600

    fix

--- s3.tf
+++ s3.tf
@@ -23,16 +23,26 @@ resource "aws_s3_bucket_ownership_controls" "logging_oc" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "logging_sse" {
   bucket = aws_s3_bucket.logging_bucket.bucket
   rule {
-    bucket_key_enabled = true
+    # bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.cmk.arn
+      sse_algorithm = "aws:kms"
+    }
   }
+  depends_on = [ aws_kms_key.cmk ]
 }
 
 #This section is to enable SSE-KMS
 resource "aws_s3_bucket_server_side_encryption_configuration" "content_sse" {
   bucket = aws_s3_bucket.content_bucket.bucket
   rule {
-    bucket_key_enabled = true
+    # bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.cmk.arn
+      sse_algorithm = "aws:kms"
+    }
   }
+  depends_on = [ aws_kms_key.cmk ]
 }
 
 # #This section is to enable SSE-KMS
@@ -50,4 +60,9 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
     suffix = "index.html"
   }
   depends_on = [ aws_s3_bucket.content_bucket ]
+}
+
+resource "aws_kms_key" "cmk" {
+  description = "This key is to encrypt the s3 buckets"
+  deletion_window_in_days = 10
 }
